a0b79e7a3800c6109a66a62e90c5b4a273e4a471
coolio321 <Properlypsychotic@protonmail.com>
2020-04-06 18:41:42 -0400

    Added volume and battery to dwm status bar, and added sudoers file.

--- dwm/setstatus.sh
+++ dwm/setstatus.sh
@@ -1,4 +1,4 @@
 while true; do
-	xsetroot -name "$(date)" 
-	sleep 1
+	xsetroot -name "  $(date | sed -e 's/\<EDT\>//g' | sed -e 's/\<AM\>//g' | sed -e 's/\<PM\>//g')  $(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2) 🔊 $(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')" 
+	sleep 0.1
 done
